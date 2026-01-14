54d5d67ed81975d4a0a48474175b80b56308b3d0
Valentin Vasilev <valentin.vasilev@sap.com>
2026-01-14 14:13:39 +0200

    :gear: Update tmux sessionizer
    Signed-off-by: Valentin Vasilev <valentin.vasilev@sap.com>

--- .config/scripts/tmux-sessionizer.sh
+++ .config/scripts/tmux-sessionizer.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
-# Use null-delimited output to handle spaces in names
+PROJECT_DIRS=("$HOME/code" "$HOME/dotfiles")
+
+set -euo pipefail
+
 selected=$(
-  find "$HOME/code" "$HOME/dotfiles" -mindepth 1 -maxdepth 1 -type d | sed -E "s|$HOME/||g" | fzf --prompt="Select directory:"
+  find "${PROJECT_DIRS[@]}" -mindepth 1 -maxdepth 1 -type d | sed -E "s|$HOME/||g" | fzf --prompt="Select project:"
 )
 
 # Exit if nothing selected
 if [[ -z "$selected" ]]; then
-  tmux display-message -d 3000 "No directory selected."
+  tmux display-message -d 3000 "No project selected."
   exit 0
 fi
 
 selected="$HOME/$selected"
 
-# basename + sanitize
+# transform name
 selected_name=$(basename "$selected")
 selected_name=${selected_name#.}    # Trim leading dot
 selected_name=${selected_name//./_} # Replace dots with underscores
@@ -30,4 +33,6 @@ if ! tmux has-session -t="$selected_name" 2>/dev/null; then
   tmux select-window -t "$selected_name:1"
 fi
 
+# switch to the session
 tmux switch-client -t "$selected_name"
+tmux display-message -d 3000 "Switched to: $selected_name"
