7232def6f729197d7c62ecc7004979c44c13ebc5
Valentin Vasilev <valentin.vasilev@sap.com>
2026-05-05 09:53:47 +0300

    :gear: Update tmux config
    Signed-off-by: Valentin Vasilev <valentin.vasilev@sap.com>

--- .config/scripts/tmux-switcher.sh
+++ .config/scripts/tmux-switcher.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 
-session=$(tmux ls 2>/dev/null | cut -d: -f1 | fzf --prompt="Select tmux session: ")
+SCRIPT=$(realpath "$0")
+current=$(tmux display-message -p '#S' 2>/dev/null)
 
-# If a session was selected, switch to it
+if [[ "$1" == "--list" ]]; then
+  tmux ls 2>/dev/null | cut -d: -f1 | awk -v cur="$current" '{
+    print ($0 == cur) ? "* " $0 : "  " $0
+  }'
+  exit 0
+fi
+
+session=$("$SCRIPT" --list | fzf \
+  --prompt="Select tmux session: " \
+  --bind="ctrl-d:execute-silent(tmux kill-session -t \$(echo {} | awk '{print \$NF}'))+reload($SCRIPT --list)" |
+  awk '{print $NF}')
+
 if [[ -n "$session" ]]; then
   tmux switch-client -t "$session"
-  exit 0
 else
   tmux display-message -d 3000 "No sessions available"
-  exit 0
 fi
